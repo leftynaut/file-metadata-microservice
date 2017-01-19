70d3594297b208cf4870f9d0ac211a811f64766d
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-01-19 09:11:00 -0600

    Added bodyparser

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const multer = require('multer');
+const bodyParser = require('body-parser')
 
 const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({storage});
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/upload', upload.single('ul'), (req, res) => {
+  console.log(req.file)
+  res.send(req.file);
 });
 
 const port = process.env.PORT || 3000;
